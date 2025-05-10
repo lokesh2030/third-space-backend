6954e0d9dd4e70b6aa201c521a9d846becbf1912
lokesh2030 <lokeshster@gmail.com>
2025-05-09 18:03:16 -0700

    Delete routes/threat.js

--- routes/threat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/threat', async (req, res) => {
-  const { keyword } = req.body;
-  res.json({ result: `Mock threat intel for: "${keyword}"` });
-});
-
-module.exports = router;
